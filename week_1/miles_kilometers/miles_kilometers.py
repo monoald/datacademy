44c49407fba615ee4a0fbc916fa8c0fc4b3fe140
Carlos Aldazosa <carlos.aldazosa@outlook.com>
2021-05-17 22:57:10 -0400

    Base of converter created

--- week_1/miles_kilometers/miles_kilometers.py
+++ week_1/miles_kilometers/miles_kilometers.py
@@ -1,5 +1,50 @@
+from os import system
+
+def validate_option():
+
+    valid_options = ['1', '2', '3', '4', '5']
+    while True:
+        try:
+            option = input('')
+
+            if option != valid_options:
+                raise ValueError('SELECT A VALID OPTION!')
+
+            break
+        except ValueError as ve:
+            system('clear')
+            print(ve)
+
+    return option
+
+
 def main():
-    pass
+
+    # Longitude in relation to 1 mile
+    longitude_miles = [{'miles': 1}, {'kilometers': 1.60934}, {'meters': 1609.34}, {'yard': 1760}, {'feet': 5280}]
+
+    options = """
+    1. Miles.
+    2. Kilometers.
+    3. Meters.
+    4. Yards.
+    5. Feets.
+    """
+
+    print(f"""Select the longitude you have:
+    {options}
+    """)
+    actual_longitude = validate_option()
+
+    print("""What is length you have?
+    """)
+    actual_length = float(input(''))
+
+    print(f"""Select the longitude you want to convert to:
+    {options}
+    """)
+    longitude_convertion = validate_option()
+
 
 if __name__ == '__main__':
     main()
