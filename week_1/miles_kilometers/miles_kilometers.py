f8a802d0ee9bc19e68892633d236346980510409
Carlos Aldazosa <carlos.aldazosa@outlook.com>
2021-05-17 23:36:52 -0400

    Exercise paused

--- week_1/miles_kilometers/miles_kilometers.py
+++ week_1/miles_kilometers/miles_kilometers.py
@@ -1,13 +1,19 @@
 from os import system
 
-def validate_option():
+def validate_option(message):
+    """Ensures to receive only a valid option.
 
+    Returns:
+    int: Valid option
+    """
     valid_options = ['1', '2', '3', '4', '5']
+
     while True:
         try:
+            print(message)
             option = input('')
 
-            if option != valid_options:
+            if option not in valid_options:
                 raise ValueError('SELECT A VALID OPTION!')
 
             break
@@ -15,13 +21,28 @@ def validate_option():
             system('clear')
             print(ve)
 
-    return option
+    return int(option)
+
+
+def convertion_miles(length, convertion_longitude):
+    """Convert from miles.
+
+    Args:
+    word (str): Word to operate
+
+    Returns:
+    str: Word without accent marks
+    """
+    return length * convertion_longitude['length']
 
 
 def main():
 
     # Longitude in relation to 1 mile
-    longitude_miles = [{'miles': 1}, {'kilometers': 1.60934}, {'meters': 1609.34}, {'yard': 1760}, {'feet': 5280}]
+    longitude_miles = ['empty',
+        {'longitude': 'miles', 'length': 1}, {'longitude': 'kilometers', 'length': 1.60934}, {'longitude': 'meters', 'length': 1609.34}, 
+        {'longitude': 'yard', 'length': 1760}, {'longitude': 'feet', 'length': 5280}
+    ]
 
     options = """
     1. Miles.
@@ -31,19 +52,36 @@ def main():
     5. Feets.
     """
 
-    print(f"""Select the longitude you have:
+    # Ask for actual longitude
+    system('clear')
+    al_message = f"""Select the longitude you have:
     {options}
-    """)
-    actual_longitude = validate_option()
+    """
+    actual_longitude = validate_option(al_message)
 
-    print("""What is length you have?
-    """)
-    actual_length = float(input(''))
+    # Ask for the length to convert
+    system('clear')
+    lc_messagae = 'What is length you have?'
+    while True:
+        try:
+            print(lc_messagae)
+            actual_length = float(input(''))
 
-    print(f"""Select the longitude you want to convert to:
+            break
+        except ValueError as ve:
+            print('THE VALUE YOU ENTER CAN NOT BE A LENGTH')
+
+    # Ask for the longitude to convert to
+    system('clear')
+    lconverter_message = f"""Select the longitude you want to convert to:
     {options}
-    """)
-    longitude_convertion = validate_option()
+    """
+    longitude_convertion = validate_option(lconverter_message)
+
+    # Convertion Logic
+    if actual_length == 1:
+        my_length = convertion_miles(actual_length, longitude_miles[longitude_convertion])
+        print(my_length)
 
 
 if __name__ == '__main__':
